970e929e5f5b81c233ce8e0ab07b47392e036390
Tim Ballaschke <tim@art-of-x.com>
2026-01-07 10:26:58 +0100

    Update project metadata and enhance image handling across various projects
    - Added new project images to multiple commissioned and personal projects for improved visual representation.
    - Updated project month and year fields to enhance clarity and consistency in project timelines.
    - Introduced a project text field to provide additional context for each project.
    - Ensured all project files maintain a consistent structure and formatting for better readability and management.

--- site/snippets/project-card.php
+++ site/snippets/project-card.php
@@ -48,5 +48,10 @@
             <div class="project-date"><?= $project->projectMonth() ?> <?= $project->projectYear() ?></div>
             <div class="square-bottom-right"></div>
         </div>
+        <?php if ($project->projectText()->isNotEmpty()): ?>
+        <div class="project-description">
+            <?= $project->projectText() ?>
+        </div>
+        <?php endif; ?>
     </div>
 </div>
